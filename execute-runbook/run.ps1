5a1f1247887157dafe70b22b2e6f205a2f00c9ff
Pradeep Kumar <pradeep.kumar@cognosante.com>
2018-01-11 14:41:58 -0500

    testing

--- execute-runbook/run.ps1
+++ execute-runbook/run.ps1
@@ -1,12 +1,9 @@
 $in = Get-Content $triggerInput -Raw
 
-$runbookName, $AutomationAccount = $in.split(",")
-$runbookName="svc-oms-automation"
-$AutomationAccount="Delete-HDISparkCluster"
+$RunbookName, $ResourceGroupName, $AutomationAccount = $in.split(",")
 
 $clientID = $env:spnid
 $key = $env:spnkey
-$resourcegroup = $env:spnresourcegroupname
 $tenantid = $env:spntenant
 $SecurePassword = $key | ConvertTo-SecureString -AsPlainText -Force
 $cred = new-object -typename System.Management.Automation.PSCredential -argumentlist $clientID, $SecurePassword
@@ -19,7 +16,7 @@ try {
     #Select-AzureSubscription -Default -SubscriptionName $env:spnsubscription
     #Select-AzureRmSubscription -SubscriptionName $env:spnsubscription
     #Start-AzureAutomationRunbook -AutomationAccountName "svc-oms-automation" -Name "Delete-HDISparkCluster"
-    Start-AzureRMAutomationRunbook -AutomationAccountName $AutomationAccount -Name $runbookName --ResourceGroupName $resourcegroup
+    Start-AzureRMAutomationRunbook -AutomationAccountName $AutomationAccount -Name $RunbookName -ResourceGroupName $ResourceGroupName
 }catch{
     $_
     $sendemail = "subject~$runbookName could not be executed~~~body~$_.Exception"
