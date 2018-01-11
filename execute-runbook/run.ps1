5872c8a811cb647626beadcb9bee61c7fec1aeea
Pradeep Kumar <pradeep.kumar@cognosante.com>
2018-01-11 12:47:29 -0500

    testing

--- execute-runbook/run.ps1
+++ execute-runbook/run.ps1
@@ -14,7 +14,7 @@ write-output "Executing Runbook $runbookName in $AutomationAccount"
 
 try {
     Add-AzureRmAccount -Credential $cred -Tenant $tenantid -ServicePrincipal -EnvironmentName AzureUSGovernment
-    Select-AzurermSubscription -SubscriptionName $env:spnsubscription
+    Select-AzureRmSubscription -SubscriptionName $env:spnsubscription
     #Start-AzureAutomationRunbook -AutomationAccountName "svc-oms-automation" -Name "Delete-HDISparkCluster"
     Start-AzureAutomationRunbook -AutomationAccountName $AutomationAccount -Name $runbookName
 }catch{
@@ -24,30 +24,3 @@ try {
 }
 $sendemail = "subject~CA-Runbook $runbookName successfully completed"
 return
-<#
-$resourceGroupName = "CERRS-DEV-TEST-RG"
-$connectionName = "AzureRunAsConnection"
-$servicePrincipalConnection=Get-AutomationConnection -Name $connectionName
-
-Add-AzureRmAccount -ServicePrincipal -TenantId $servicePrincipalConnection.TenantId -ApplicationId $servicePrincipalConnection.ApplicationId -CertificateThumbprint $servicePrincipalConnection.CertificateThumbprint -EnvironmentName AzureUSGovernment
-
-
-Start-AzureRmAutomationRunbook -Name "Delete-HDISparkCluster" -AutomationAccountName "svc-oms-automation" -ResourceGroupName "CERRS-DEV-TEST-RG"
-#>
-
-<#
-$runbookName = "Delete-HDISparkCluster"
-$ResourceGroup = "CERRS-DEV-TEST-RG"
-$AutomationAcct = "svc-oms-automation"
-
-$job = Start-AzureRmAutomationRunbook –AutomationAccountName $AutomationAcct -Name $runbookName -ResourceGroupName $ResourceGroup
-
-$doLoop = $true
-While ($doLoop) {
-   $job = Get-AzureRmAutomationJob –AutomationAccountName $AutomationAcct -Id $job.JobId -ResourceGroupName $ResourceGroup
-   $status = $job.Status
-   $doLoop = (($status -ne "Completed") -and ($status -ne "Failed") -and ($status -ne "Suspended") -and ($status -ne "Stopped"))
-}
-
-Get-AzureRmAutomationJobOutput –AutomationAccountName $AutomationAcct -Id $job.JobId -ResourceGroupName $ResourceGroup –Stream Output
-#>
