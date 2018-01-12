b4d20ba8c993da9ade6bcb2573c26d0e7a838883
Pradeep Kumar <pradeep.kumar@cognosante.com>
2018-01-11 22:05:23 -0500

    testting

--- Listener/run.ps1
+++ Listener/run.ps1
@@ -1,4 +1,5 @@
 $in = Get-Content $triggerInput -Raw
+$proj, $event = $in.split("-")
 
 $clientID = $env:spnid
 $key = $env:spnkey
@@ -8,12 +9,14 @@ $cred = new-object -typename System.Management.Automation.PSCredential -argument
 $resourceGroupName = "cerrs-dev-test-rg"
 $StorageAccountName = "cerrscaseautomation"
 $container="listemer"
-$filename="listener.json"
+$filename="$proj.json"
 
 $storagekey="g4cMymRd43HdRoAU+nHVNVUInozYYu8yE8Yo7QG3Jfe0namaWmeKCL6zD4BsKjdsLZGRSmjk7Ez0mJ4aa6S2wA=="
 
 $ctx=New-AzureStorageContext -StorageAccountName $StorageAccountName -StorageAccountKey $storagekey -EnvironmentName "AzureUSGovernment"
 #$StorageAccount = Get-AzureRmStorageAccount -ResourceGroupName $ResourceGroupName -Name $StorageAccountName
 $Blob = Get-AzureStorageBlob -Context $ctx -Container $container -Blob $filename
-$Text = $blob.ICloudBlob.DownloadText()
-Write-Output $Text
+$listenerjson = $blob.ICloudBlob.DownloadText() | ConvertTo-Json
+
+
+Write-Output $listenerjson
