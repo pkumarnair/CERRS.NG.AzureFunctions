66b07ac47d90a6025ce2ac486aae98180115f467
Pradeep Kumar <pradeep.kumar@cognosante.com>
2018-01-31 18:55:55 -0500

    test

--- modules/Execute-Runbook.psm1
+++ modules/Execute-Runbook.psm1
@@ -6,21 +6,35 @@ function Execute-Runbook{
 
     )
 
+    $AutomationAccount=""
+    $RunbookName=""
+    $ResourceGroupName=""
     $RbParams=@{}
 
-    ForEach($_ in $RbParamsIn){
+    write-output "Starting Execute-Runbook------------"
+    write-output $params
+
+    $RbParams=@{}
+
+    ForEach($_ in $params){
         $key,$value=$_.split("=")
         if($key="runbookName"){
-          $runbookName=$value
+          $RunbookName=$value
         }ElseIf($key="resourceGroup"){
-          $resourceGroup=$value
+          $ResourceGroupName=$value
         }ElseIf($key="automationAccount"){
-          $automationAccount=$value
+          $AutomationAccount=$value
         }else{
 
         }
         $RbParams.add($_.split("=")[0],$value)
     }
+
+    write-output "The Runbook job parameters ------------"
+    write-output $RunbookName
+    write-output $AutomationAccount
+    write-output $ResourceGroupName
+    write-output $RbParams
 
     $clientID = $env:spnid
     $key = $env:spnkey
@@ -28,8 +42,7 @@ function Execute-Runbook{
     $SecurePassword = $key | ConvertTo-SecureString -AsPlainText -Force
     $cred = new-object -typename System.Management.Automation.PSCredential -argumentlist $clientID, $SecurePassword
 
-    write-output "Executing Runbook $runbookName in $AutomationAccount"
-    write-output $RbParams
+    write-output "Executing Runbook $RunbookName in $AutomationAccount"
 
     try {
         Add-AzureRmAccount -Credential $cred -Tenant $tenantid -ServicePrincipal -EnvironmentName AzureUSGovernment
