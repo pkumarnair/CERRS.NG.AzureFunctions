e21bcef857d02bd3cbca901f10a20dfd459d4443
Pradeep Kumar <pradeep.kumar@cognosante.com>
2018-02-02 10:08:00 -0500

    test

--- modules/Initiate-Parallel-Requests.psm1
+++ modules/Initiate-Parallel-Requests.psm1
@@ -1,5 +1,3 @@
-Import-Module Import-Module "D:\home\site\wwwroot\modules\AzureRmStorageQueueCoreHelper.psm1"
-
 function Initiate-Parallel-Requests{
     Param
     (
@@ -14,20 +12,6 @@ function Initiate-Parallel-Requests{
     $connectionstring=""
     $messages=@()
     write-output "Inside Initiate-Parallel-Requests------------"
-
-    $resourcegroupname=$env:spnresourcegroupname
-    $clientID = $env:spnid
-    $key = $env:spnkey
-    $tenantid = $env:spntenant
-    $SecurePassword = $key | ConvertTo-SecureString -AsPlainText -Force
-    $cred = new-object -typename System.Management.Automation.PSCredential -argumentlist $clientID, $SecurePassword
-
-    try {
-        Add-AzureRmAccount -Credential $cred -Tenant $tenantid -ServicePrincipal -EnvironmentName AzureUSGovernment
-    }catch{
-        $_
-        return
-    }
 
     ForEach($_ in $params.split("~")){
         $key,$val=$_.split("=")
@@ -55,38 +39,62 @@ function Initiate-Parallel-Requests{
         }
     }
 
-
-    $storage=Get-AzureRmStorageAccount -ResourceGroupName $resourcegroupname -AccountName $storageaccountname
-
-    write-output $storage
     write-output "queuename is $queuename"
     write-output "connectionstring is $connectionstring"
     write-output "storageaccountname is $storageaccountname"
+    write-output "storagekey is $storagekey"
     write-output "messages is $messages"
-
+<#
     try{
+        $message=$proj+"-"+$message
+        test-function $storageaccountname $storagekey $queuename $message 
         write-output "11111111111111111111111111111111111111111"
-        #$ctx=New-AzureStorageContext -StorageAccountName $storageaccountname -StorageAccountKey $storagekey -Environment AzureUSGovernment
+        $ctx=New-AzureStorageContext -StorageAccountName $storageaccountname -StorageAccountKey $storagekey -Environment AzureUSGovernment
         #$ctx=New-AzureStorageContext -ConnectionString $connectionstring
-        #$ctx=$storage.Context
         write-output "22222222222222222222222222222222222222222"
-        #$queue = Get-AzureStorageQueue –Name $queuename –Context $ctx
-        $queue = Get-AzureRmStorageQueueQueue -resourceGroup $resourcegroupname -storageAccountName $storageaccountname -queueName $queuename
+        $queue = Get-AzureStorageQueue –Name $queuename –Context $ctx
     }catch{
         $_
         return
     }
-
+#>
     ForEach($message in $messages){
         try {
-            write-output "3333333333333333333333333333333333333333333"
-            $queueMessage = New-Object -TypeName Microsoft.WindowsAzure.Storage.Queue.CloudQueueMessage -ArgumentList ($proj+"-"+$message)
+            $message=$proj+"-"+$message
+            test-function $storageaccountname $storagekey $queuename $message 
+            #write-output "3333333333333333333333333333333333333333333"
+            #$queueMessage = New-Object -TypeName Microsoft.WindowsAzure.Storage.Queue.CloudQueueMessage -ArgumentList ($proj+"-"+$message)
             #$queue.CloudQueue.AddMessage($queueMessage)
-            Add-AzureRmStorageQueueMessage -queue $queue -message @{"message"=$proj+"-"+$message}
         }catch{
             $_
             return
         }
+    }
+
+    return
+}
+
+function test-function{
+    params(
+        $storageaccountname,
+        $storagekey,
+        $queuename,
+        $message
+
+        )
+
+    try{
+        write-output "11111111111111111111111111111111111111111"
+        $ctx=New-AzureStorageContext -StorageAccountName $storageaccountname -StorageAccountKey $storagekey -Environment AzureUSGovernment
+        write-output "22222222222222222222222222222222222222222"
+        $queue = Get-AzureStorageQueue –Name $queuename –Context $ctx
+        write-output "33333333333333333333333333333333333333333"
+        $queueMessage = New-Object -TypeName Microsoft.WindowsAzure.Storage.Queue.CloudQueueMessage -ArgumentList $message
+        write-output "44444444444444444444444444444444444444444"
+        $queue.CloudQueue.AddMessage($queueMessage)
+    }catch{
+        $_
+        return
     }
 
     return
